65bb59eaf28bce771e74de640252c8d092e3b911
jacobsaxa <jacobsaxa@gmail.com>
2025-09-16 17:21:46 -0500

    Add details to weather display and add css formating.

--- src/Components/current-weather/current-weather.js
+++ src/Components/current-weather/current-weather.js
@@ -4,10 +4,33 @@ const CurrentWeather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Chicago</p>
-                <p className="weather-description">Sunny</p>
+                <div>
+                    <p className="city">Chicago</p>
+                    <p className="weather-description">Sunny</p>
+                </div>
+                <img alt="weather" className="weather-icon" src="icons/01d.png" />
             </div>
-            <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+            <div className="bottom">
+                <p className="temperature">85°</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels Like</span>
+                        <span className="parameter-label">78°</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-label">25 mph</span>
+                    </div>
+                     <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-label">60%</span>
+                    </div>
+                     <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-label">10hPa</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
